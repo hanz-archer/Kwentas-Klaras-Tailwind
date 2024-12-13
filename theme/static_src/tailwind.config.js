c0c35cd53761d21bf2676a52e073a26d22f26c5a
Hans Archer Montezon Dalubatan <dalubatanhans@gmail.com>
2024-12-14 00:15:47 +0800

    Second

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -42,7 +42,11 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
+        extend: {
+            transitionProperty: {
+                height: 'height',
+            },
+        },
     },
     plugins: [
         /**
